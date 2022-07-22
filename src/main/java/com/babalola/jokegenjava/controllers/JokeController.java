0edd17c5c0f9728411b1bb54ac9a8906d001d74d
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2022-07-22 21:11:20 +0100

    Added controller

---
+++ src/main/java/com/babalola/jokegenjava/controllers/JokeController.java
@@ -0,0 +1,21 @@
+package com.babalola.jokegenjava.controllers;
+
+import com.babalola.jokegenjava.services.JokeService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/"})
+    public String displayJoke(Model model){
+    model.addAttribute("joke", jokeService.getJoke());
+
+    return "index";
+    }
+}
