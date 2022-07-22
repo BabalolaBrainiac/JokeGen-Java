5a5ff5216b4d7a63db1b6aeb69156dd915901c6f
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2022-07-22 21:26:32 +0100

    Added Thymeleaf view for index page

--- src/main/java/com/babalola/jokegenjava/controllers/JokeController.java
+++ src/main/java/com/babalola/jokegenjava/controllers/JokeController.java
@@ -1,9 +1,11 @@
 package com.babalola.jokegenjava.controllers;
 
 import com.babalola.jokegenjava.services.JokeService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class JokeController {
 
     private final JokeService jokeService;
